2601a250d83cea07370d666404834680b5e9d246
wangboo <advance345@126.com>
2014-06-27 16:27:38 +0800

    add qiubai_spidr writen in ruby

--- qiubai-spidr/qiubai_spidr.rb
+++ qiubai-spidr/qiubai_spidr.rb
@@ -5,30 +5,51 @@ require 'nokogiri'
 require 'fileutils'
 # require 'mongoid_auto_increment_id'
 
+def grep body, p
+  doc = Nokogiri::HTML(body)
+  # puts doc
+  index = 1
+  doc.css('.article').each do |item|
+    qdata = {from: "qiubai"}
+    id = item["id"].gsub(/\d+/).first
+    content = item.css('.content').text
+    content = content.encode('utf-8', 'utf-8').chomp
+
+    qdata[:base_id] = id
+    qdata[:content] = content
+    thumb = item.css('.thumb a img').first
+    
+    if thumb
+      qdata[:pic_url] = thumb['src']
+    end
+    puts "第#{p}页--第#{index}条 "
+    HTTParty.post('http://192.168.4.56:3001/node/joke/new', {query: qdata, headers: {"Content-Type" => 'utf-8'}})
+    puts qdata
+    index += 1
+  end
+end
+
 
 AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.13'
 
 page = ARGV.shift || '1'
 page = page.to_i
 
-page.upto(page+100) do |p|
+page.upto(page+10000) do |p|
   puts "grep http://www.qiushibaike.com/8hr/page/#{p}?s=4674024"
-  body = HTTParty.get("http://www.qiushibaike.com/8hr/page/#{p}?s=4674024", :headers => {"User-Agent" => AGENT}).body
-  doc = Nokogiri::HTML(body)
-  # puts doc
-  index = 1
-  doc.css('.article').each do |item|
-    id = item["id"]
-    content = item.css('.content')
-    thumb = item.css('.thumb a img').first
-    img = nil
-    if thumb
-      img = thumb['src']
+  repeat_times = 0
+  body = nil
+  while
+    begin
+      body = HTTParty.get("http://www.qiushibaike.com/8hr/page/#{p}?s=4674024", :headers => {"User-Agent" => AGENT}).body
+      break
+    rescue => e
+      if repeat_times>5 then break end
+      repeat_times += 1
+      sleep 5
     end
-    puts "第#{p}页--第#{index}条"
-    puts "#{content.text}, img=#{img}"
-    HTTParty.get('127.0.0.1:3001/node/joke/new', {})
-    index += 1
-    sleep 1
   end
+  if body then grep(body, p) else p += 1 end
+  sleep 5
 end
+
